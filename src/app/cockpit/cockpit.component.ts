1690b3a9b0987626f473c311e183d00973121c02
Apoorva Shah <apoorvashah28@gmail.com>
2017-09-01 20:53:47 +0530

    Getting access to local reference by ViewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,24 +9,26 @@ export class CockpitComponent implements OnInit {
     @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
     @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
-    newServerContent = '';
+    //newServerContent = '';
+
+  @ViewChild('serverContentInput') serverContentInput : ElementRef;
   constructor() { }
 
   ngOnInit() {
   }
 
     onAddServer(nameInput: HTMLInputElement) {
-        console.log(nameInput.value);
         this.serverCreated.emit({
             serverName: nameInput.value,
-            serverContent: this.newServerContent
+            serverContent: this.serverContentInput.nativeElement.value
         });
     }
 
     onAddBlueprint(nameInput: HTMLInputElement) {
+        this.serverContentInput.nativeElement.value = 'something';
         this.blueprintCreated.emit({
             serverName: nameInput.value,
-            serverContent: this.newServerContent
+            serverContent: this.serverContentInput.nativeElement.value
         });
     }
 }
